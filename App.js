7a33a27ee0e6994c8d6cdf7260cb687bc7008b00
Justinas <j.zailskas@eif.viko.lt>
2020-10-21 17:23:50 +0300

    Import Provider

--- App.js
+++ App.js
@@ -5,21 +5,31 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import showScreen from './src/screens/showScreen';
 import addScreen from './src/screens/addScreen';
 import deleteScreen from './src/screens/deleteScreen';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import combineReducers from './store/reducers/index';
+import {addCar} from './store/actions/actions';
 
 const Tab = createBottomTabNavigator();
-
+const store = createStore(combineReducers);
+console.log('Before', store.getState());
+store.dispatch(addCar('BMW', 'M3', 1));
+store.dispatch(addCar('BMW', 'M5', 2));
+console.log('After', store.getState());
 class App extends Component {
   render() {
     return (
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRoute="showMainInformation"
-          tabBarOptions={{activeTintColor: '#deaf04'}}>
-          <Tab.Screen name="SHOW" component={showScreen} />
-          <Tab.Screen name="ADD" component={addScreen} />
-          <Tab.Screen name="DELETE" component={deleteScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Tab.Navigator
+            initialRoute="showMainInformation"
+            tabBarOptions={{activeTintColor: '#deaf04'}}>
+            <Tab.Screen name="SHOW" component={showScreen} />
+            <Tab.Screen name="ADD" component={addScreen} />
+            <Tab.Screen name="DELETE" component={deleteScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </Provider>
     );
   }
 }
